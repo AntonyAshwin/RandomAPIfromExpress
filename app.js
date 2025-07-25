d70b29f37a88a7adbc7c07c4297643f8dc64b035
Antony Ashwin <antonyashwin555@gmail.com>
2025-07-25 22:08:00 +0530

    add express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello, World!' });
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
